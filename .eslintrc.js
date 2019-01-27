41ba2c8ae9c6ec3a1ed3733529cdcd0f962ae9c4
Klaudia Alvarez <klaudia.devel@gmail.com>
2019-01-27 23:04:31 +0100

    add ESlint support for Gridsome

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  plugins: ['gridsome'],
+  rules: {
+    'gridsome/format-query-block': 'error',
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  useTabs: false
+}
