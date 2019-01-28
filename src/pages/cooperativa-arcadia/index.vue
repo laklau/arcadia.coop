c45dd0af9185c0d1a0d0afecee5e208aefaa87ff
Klaudia Alvarez <klaudia.devel@gmail.com>
2019-01-28 12:08:55 +0100

    cooperativa-arcadia (icon blocs): create section markup

--- src/pages/cooperativa-arcadia/index.vue
+++ src/pages/cooperativa-arcadia/index.vue
@@ -19,6 +19,60 @@
           </div>
         </div>
       </section>
+      <section class="flex justify-center flex-wrap lg:flex-no-wrap">
+        <div class="flex-grow text-center pt-12 pb-12 sm:pt-24 sm:pb-24">
+          <div class="m-auto mb-6 w-16 h-16">
+            <i>
+              <img src="/assets/icons/liniaprogresiva.svg" alt="icon">
+            </i>
+          </div>
+          <h4
+            class="pb-8 text-lg sm:text-xl text-grey-darker font-semibold leading-tight"
+          >Una sola línia progressiva</h4>
+          <p class="pl-8 pr-8 sm:pl-12 sm:pr-12 text-base text-grey-dark leading-normal">
+            L'Arcàdia és una escola petita, d'una sola línia, que iniciarà una
+            implementació progressiva des d'infantil. Aquest procés començarà
+            amb tots els grups d'infantil (3,4 i 5 anys) i pel següent curs el
+            grup de cinc anys obriran el primer curs de primària, alhora que
+            entrarà una nova generació de tres anys i així successivament.
+          </p>
+        </div>
+        <div class="flex-grow bg-red-dark text-center pt-12 pb-12 sm:pt-24 sm:pb-24">
+          <div class="m-auto mb-6 w-16 h-16">
+            <i>
+              <img src="/assets/icons/aprenentatgeautonom.svg" alt="icon">
+            </i>
+          </div>
+          <h4
+            class="pb-8 text-lg sm:text-xl text-white font-semibold leading-tight"
+          >Aprenentatge autònom</h4>
+          <p class="pl-8 pr-8 sm:pl-12 sm:pr-12 text-base text-white leading-normal">
+            El dia a dia d’aquesta escola s’organitza a partir de l’Assemblea i
+            les cooperatives escolars, és a dir, que els nens i les nenes
+            aprenen a gestionar les seves vides de manera autònoma i
+            participativa, fent-se responsables de les decisions que lliurement
+            han acordat, desenvolupant alhora un fort sentiment de solidaritat.
+          </p>
+        </div>
+        <div class="flex-grow text-center pt-12 pb-12 sm:pt-24 sm:pb-24">
+          <div class="m-auto mb-6 w-16 h-16">
+            <i>
+              <img src="/assets/icons/educaciocritica.svg" alt="icon">
+            </i>
+          </div>
+          <h4
+            class="pb-8 text-lg sm:text-xl text-grey-darker font-semibold leading-tight"
+          >Educació crítica i participativa</h4>
+          <p class="pl-8 pr-8 sm:pl-12 sm:pr-12 text-base text-grey-dark leading-normal">
+            Es tracta d’aprendre a aprendre. És una visió constructivista del
+            coneixement que posa l’accent a la pròpia activitat de la persona,
+            els seus interessos i els seus ritmes. D’altra banda, l’Arcàdia
+            cerca l’aprenentatge de la cooperació, entenent que tota persona
+            necessita la satisfacció d’una vida activa i socialment
+            participativa.
+          </p>
+        </div>
+      </section>
     </main>
   </Layout>
 </template>
