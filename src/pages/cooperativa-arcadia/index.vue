a2a57776799e676c812c0bce55b766713cf37881
Klaudia Alvarez <klaudia.devel@gmail.com>
2019-01-28 12:06:33 +0100

    cooperativa-arcadia (intro): create section markup

--- src/pages/cooperativa-arcadia/index.vue
+++ src/pages/cooperativa-arcadia/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <Layout></Layout>
+  <Layout>
+    <main>
+      <section
+        class="flex justify-center pr-4 pl-4 sm:pr-32 sm:pl-32 pt-20 sm:pt-32 pb-20 sm:pb-32 bg-grey-lightest"
+      >
+        <div class="text-center xl:pr-32 xl:pl-32 xl:mr-16 xl:ml-16">
+          <h2
+            class="pb-8 text-2xl sm:text-3xl text-red-dark font-semibold leading-tight"
+          >Centre Educatiu autogestionari, cooperatiu i comunitari</h2>
+          <div class="text-lg sm:text-xl text-grey-darker font-light leading-normal">
+            <p>
+              El projecte de cooperativa Arcàdia cerca solucions socials que no
+              passen per la integració a la cultura dominant i té com objectiu
+              impulsar una proposta d’escola dintre d’un marc de renovació
+              pedagògica que fomenti persones autònomes amb ments crítiques i
+              flexibles.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  </Layout>
 </template>
 
 <script>
