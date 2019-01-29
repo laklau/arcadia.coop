1a964a8628111b6a0a86d18abe2ff5fb4ec405c1
Klaudia Alvarez <klaudia.devel@gmail.com>
2019-01-29 11:54:26 +0100

    funcionament (intro): create section markup

---
+++ src/pages/funcionament.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    <main>
+      <section
+        class="flex justify-center pr-4 pl-4 sm:pr-32 sm:pl-32 pt-20 sm:pt-32 pb-20 sm:pb-32 bg-grey-lightest"
+      >
+        <div class="text-center xl:pr-32 xl:pl-32 xl:mr-16 xl:ml-16">
+          <h2
+            class="pb-8 text-2xl sm:text-3xl text-red-dark font-semibold leading-tight"
+          >Un projecte pedagògic triplement autogestionat</h2>
+          <div class="text-lg sm:text-xl text-grey-darker font-light leading-normal">
+            <p>L’escola Arcàdia és un projecte autogestionari, cooperatiu i comunitari d’educació en i per la llibertat responsable, que comprèn les etapes d’educació infantil, primària i secundària obligatòria i què assumeix els principis d’autogestió de Can Batlló a tres nivells</p>
+          </div>
+        </div>
+      </section>
+    </main>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
