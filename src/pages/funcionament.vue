e9e49c55bbf31e3380983f457211141757f18df3
Klaudia Alvarez <klaudia.devel@gmail.com>
2019-01-29 12:16:15 +0100

    funcionament (icons section): create section markup

--- src/pages/funcionament.vue
+++ src/pages/funcionament.vue
@@ -2,7 +2,7 @@
   <Layout>
     <main>
       <section
-        class="flex justify-center pr-4 pl-4 sm:pr-32 sm:pl-32 pt-20 sm:pt-32 pb-20 sm:pb-32 bg-grey-lightest"
+        class="flex justify-center flex-wrap px-4 sm:px-32 pt-20 sm:pt-32 pb-20 sm:pb-32 bg-grey-lightest"
       >
         <div class="text-center xl:pr-32 xl:pl-32 xl:mr-16 xl:ml-16">
           <h2
@@ -10,6 +10,41 @@
           >Un projecte pedagògic triplement autogestionat</h2>
           <div class="text-lg sm:text-xl text-grey-darker font-light leading-normal">
             <p>L’escola Arcàdia és un projecte autogestionari, cooperatiu i comunitari d’educació en i per la llibertat responsable, que comprèn les etapes d’educació infantil, primària i secundària obligatòria i què assumeix els principis d’autogestió de Can Batlló a tres nivells</p>
+          </div>
+        </div>
+        <div class="flex justify-center flex-wrap lg:flex-no-wrap">
+          <div class="flex-1 py-8 sm:pt-12 sm:pb-0">
+            <div class="mb-6 w-16 h-16">
+              <img src alt="icon">
+            </div>
+            <h5
+              class="px-8 sm:px-12 -ml-8 sm:-ml-12 pb-8 text-grey-darker font-semibold text-base leading-tight"
+            >Autogestió en la gestió</h5>
+            <p
+              class="px-8 sm:px-12 -ml-8 sm:-ml-12 text-base text-grey-dark leading-normal"
+            >La gestió del centre està dissenyada seguint els criteris autogestionaris i la legalitat vigent, mitjançant una cooperativa sense ànim de lucre. Aquesta cooperativa es constitueix amb mecanismes de radicalitat laboral, com la supressió d'escales salarials, l'assumpció de l'assemblea com a únic òrgan decisori i una política interna d'economia feminista. I tot això coordinat orgànicament amb l'Impuls cooperatiu de Sants i l'assemblea General de Can Batlló a la que rendeix comptes anualment.</p>
+          </div>
+          <div class="flex-1 py-8 sm:pt-12 sm:pb-0">
+            <div class="mb-6 w-16 h-16">
+              <img src alt="icon">
+            </div>
+            <h5
+              class="px-8 sm:px-12 -ml-8 sm:-ml-12 pb-8 text-grey-darker font-semibold text-base leading-tight"
+            >Autogestió en l'organització</h5>
+            <p
+              class="px-8 sm:px-12 -ml-8 sm:-ml-12 text-base text-grey-dark leading-normal"
+            >A l'Arcàdia, l'organització del centre està en mans dels nens i nenes, l'equip educatiu i l'entorn familiar i comunitari. Organitzada l'escola, per diferents assemblees, comissions, cooperatives escolars i grups naturals, tots ells vinculats entre si, de manera orgànica i assembleària, tenen capacitat per decidir sobre tots aquells aspectes que els incumbeixen el seu quotidià. L'objectiu és desenvolupar un entorn escolar versàtil des d'on emergeixin els significats compartits d'una cultura escolar dialògica.</p>
+          </div>
+          <div class="flex-1 py-8 sm:pt-12 sm:pb-0">
+            <div class="mb-6 w-16 h-16">
+              <img src alt="icon">
+            </div>
+            <h5
+              class="px-8 sm:px-12 -mx-8 sm:-mx-12 pb-8 text-grey-darker font-semibold text-base leading-tight"
+            >Autogestió en l'aprenentatge</h5>
+            <p
+              class="px-8 sm:px-12 -mx-8 sm:-mx-12 text-base text-grey-dark leading-normal"
+            >Adquirir destreses pel control del propi procés d'aprenentatge és la fita que s'exerceix des de ben petits i petites, sempre respectant els diferents ritmes i el moment maduratiu de cada persona. Això ho fem progressant en una autoimatge ajustada, gaudint en tot moment del procés i acceptant l'error com a part constituent del mateix. Així, poc a poc, el nen i la nena van aprenent a retardar la gratificació i a desenvolupar un fort sentiment de plaer per l'aprenentatge autònom, el coneixement i el pensament crític.</p>
           </div>
         </div>
       </section>
