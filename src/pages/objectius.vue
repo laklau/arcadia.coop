8e5b34088738017b8d2c6fb9062d05c3792c57ce
Klaudia Alvarez <klaudia.devel@gmail.com>
2019-01-29 11:19:34 +0100

    objectius (icons section): create section markup

---
+++ src/pages/objectius.vue
@@ -0,0 +1,56 @@
+<template>
+  <Layout>
+    <main>
+      <section class="px-4 sm:px-32 pt-20 sm:pt-32 pb-20 sm:pb-32 bg-grey-lightest">
+        <h2
+          class="text-2xl sm:text-3xl text-center text-green-dark font-semibold leading-tight"
+        >L’Arcàdia treballa en tres àmbits ben definits:</h2>
+        <div class="flex justify-center flex-wrap lg:flex-no-wrap">
+          <div class="flex-grow py-8 sm:pt-12 sm:pb-0">
+            <div class="mb-6 w-16 h-16">
+              <img src="/assets/icons/entornfamiliar.svg" alt="icon">
+            </div>
+            <h5
+              class="px-8 sm:px-12 -ml-8 sm:-ml-12 pb-8 text-grey-darker font-semibold text-base leading-tight"
+            >L'entorn familiar</h5>
+            <p class="px-8 sm:px-12 -ml-8 sm:-ml-12 text-base text-grey-dark leading-normal">
+              Oferir una atenció, servei i possibilitat de participació segons necessitats i interessos, sempre vetllant amb especial cura i sensibilitat per a aquelles
+              famílies que per la
+              seva situació ho requereixin.
+            </p>
+          </div>
+          <div class="flex-grow py-8 sm:pt-12 sm:pb-0">
+            <div class="mb-6 w-16 h-16">
+              <img src="/assets/icons/desenvolupamentinfancia.svg" alt="icon">
+            </div>
+            <h5
+              class="px-8 sm:px-12 -ml-8 sm:-ml-12 pb-8 text-grey-darker font-semibold text-base leading-tight"
+            >El desenvolupament de la infància</h5>
+            <p class="px-8 sm:px-12 -ml-8 sm:-ml-12 text-base text-grey-dark leading-normal">
+              Crear un entorn físic i humà de plena acceptació on cada nen i cada nena pugui desenvolupar lliurement el dret irrenunciable a construir el seu propi relat
+              de vida sense
+              ingerències dogmàtiques ni prejudicis.
+            </p>
+          </div>
+          <div class="flex-grow py-8 sm:pt-12 sm:pb-0">
+            <div class="mb-6 w-16 h-16">
+              <img src="/assets/icons/entornsocial.svg" alt="icon">
+            </div>
+            <h5
+              class="px-8 sm:px-12 -mx-8 sm:-mx-12 pb-8 text-grey-darker font-semibold text-base leading-tight"
+            >L' entorn social</h5>
+            <p class="px-8 sm:px-12 -mx-8 sm:-mx-12 text-base text-grey-dark leading-normal">
+              Cercar la participació i el consens social sobre la base d'una ètica de mínims a partir d'una comunitat deliberativa instrumental com a mecanisme
+              d'intersecció social de les
+              diferents realitats de l'entorn.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
